48e6ca9be8c8162b734f1687f4db29d2594b4df8
B1329025 <B1329025@cgu.edu.tw>
2026-05-13 13:49:13 +0800

    Add files via upload

---
+++ ex#12/react_login/App.js
@@ -0,0 +1,16 @@
+import CGU_Login from './cgu_login';
+import './App.css';
+import MultiButton from './cgu_multiButton'
+import HelloCGU  from './cgu_hello';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {CGU_Login()}
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
